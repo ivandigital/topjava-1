6346af4b9b2d4402f36972d740265cf8b3ce5548
Ivan Digital <ivndgtl@gmail.com>
2019-10-15 18:24:47 +0300

    Business logic of meals

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -2,7 +2,10 @@ package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Collection;
+import java.util.List;
 
 public interface MealRepository {
     // null if not found, when updated
@@ -14,5 +17,15 @@ public interface MealRepository {
     // null if not found
     Meal get(int id);
 
+    // get all meals
     Collection<Meal> getAll();
+
+    List<Meal> getAllByUser(int userId);
+
+    // get meals in date peridd
+    List<Meal> getByPeriod(LocalDate dateStart, LocalDate dateEnd, int userId);
+
+    // get meals in time period
+    List<Meal> getByPeriod(LocalTime timeStart, LocalTime timeEnd, int userId);
+
 }
--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,63 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.*;
+
+@Service
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
+
+    @Autowired
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(Meal meal, int userId) {
+        checkNew(meal);
+        assureUserMealConsistent(userId, meal);
+        return repository.save(meal);
+    }
+
+    public Meal get(int id, int userId) {
+        Meal meal = repository.get(id);
+        assureUserMealConsistent(userId, meal);
+        return meal;
+///        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    public Meal update(Meal meal, int userId) {
+        checkNotNew(meal);
+        checkNotFoundWithId(repository.get(meal.getId()), meal.getId());
+        assureUserMealConsistent(userId, meal);
+        return repository.save(meal);
+    }
+
+    public void delete(int id, int userId) {
+        Meal meal = repository.get(id);
+        assureUserMealConsistent(userId, meal);
+        repository.delete(id);
+///        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public List<Meal> getAll(int userId) {
+        return repository.getAllByUser(userId);
+    }
+
+    public List<Meal> getByPeriod(LocalDate dateStart, LocalDate dateEnd, int userId) {
+        return repository.getByPeriod(dateStart, dateEnd, userId);
+    }
+
+    public List<Meal> getByPeriod(LocalTime timeStart, LocalTime timeEnd, int userId) {
+        return repository.getByPeriod(timeStart, timeEnd, userId);
+    }
 
 }
