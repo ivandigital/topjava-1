930d9cc6e73cd0ab4778dba9a6ea393e5dfb8c40
Ivan Digital <ivndgtl@gmail.com>
2019-10-28 17:05:24 +0300

    JPA entity for meals

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,43 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQuery(name = Meal.READ, query = "SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:user_id")
+@NamedQuery(name = Meal.READ_ALL, query = "SELECT m FROM Meal m WHERE m.user.id=:user_id ORDER BY m.dateTime DESC")
+@NamedQuery(name = Meal.READ_BY_INTERVAL, query = "SELECT m FROM Meal m WHERE m.user.id=:user_id AND m.dateTime>=:startDate AND m.dateTime<=:endDate ORDER BY m.dateTime DESC")
+@NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:user_id")
+
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(name = "meals_unique_idx", columnNames = {"user_id", "date_time"})})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String READ             = "Meal.get";
+    public static final String READ_ALL         = "Meal.getAll";
+    public static final String READ_BY_INTERVAL = "Meal.getByInterval";
+    public static final String DELETE           = "Meal.delete";
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @NotNull
+    @Size(min = 1, max = 100)
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @NotNull
+    @PositiveOrZero
     private int calories;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @NotNull
     private User user;
 
     public Meal() {
@@ -72,11 +96,10 @@ public class Meal extends AbstractBaseEntity {
 
     @Override
     public String toString() {
-        return "Meal{" +
-                "id=" + id +
-                ", dateTime=" + dateTime +
-                ", description='" + description + '\'' +
-                ", calories=" + calories +
-                '}';
+        return String.format(
+                "Meal {id=%d, dateTime=%s, description='%s', calories=%d, user_id=%s}",
+                id, dateTime, description, calories, user!=null ? user.getId() : "<NULL>"
+        );
     }
+
 }
