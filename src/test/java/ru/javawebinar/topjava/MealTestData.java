56be201d64fdc62a6ead3d32c58854aac73ae767
Ivan Digital <ivndgtl@gmail.com>
2019-10-29 18:21:32 +0300

    Set test-users to test meals

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -25,16 +25,29 @@ public class MealTestData {
 
     public static final List<Meal> MEALS = Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1);
 
+    // static constructor
+    static {
+        // set user of meals
+        MEALS.forEach(meal -> meal.setUser(UserTestData.USER));
+        Arrays.asList(ADMIN_MEAL1, ADMIN_MEAL2).forEach(meal -> meal.setUser(UserTestData.ADMIN));
+    }
+
     public static Meal getCreated() {
-        return new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Созданный ужин", 300);
+        Meal meal = new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Созданный ужин", 300);
+        meal.setUser(UserTestData.USER);
+        return meal;
+///        return new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Созданный ужин", 300);
     }
 
     public static Meal getUpdated() {
-        return new Meal(MEAL1_ID, MEAL1.getDateTime(), "Обновленный завтрак", 200);
+        Meal meal = new Meal(MEAL1_ID, MEAL1.getDateTime(), "Обновленный завтрак", 200);
+        meal.setUser(UserTestData.USER);
+        return meal;
+///        return new Meal(MEAL1_ID, MEAL1.getDateTime(), "Обновленный завтрак", 200);
     }
 
     public static void assertMatch(Meal actual, Meal expected) {
-        assertThat(actual).isEqualToComparingFieldByField(expected);
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "user");
     }
 
     public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
@@ -42,6 +55,6 @@ public class MealTestData {
     }
 
     public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
-        assertThat(actual).usingFieldByFieldElementComparator().isEqualTo(expected);
+        assertThat(actual).usingElementComparatorIgnoringFields("user").isEqualTo(expected);
     }
 }
